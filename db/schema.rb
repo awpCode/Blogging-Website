0eada41a8ffea2f2f21ce5b35f8059973079e04f
awpCode <rishabh.jaypee@gmail.com>
2020-08-31 17:26:10 +0530

    added_articles_show_functionality

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_094125) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description "
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
