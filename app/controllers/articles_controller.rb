bee12470e5a8b79808bd01b183ab0da7614464d1
awpCode <rishabh.jaypee@gmail.com>
2020-09-01 00:40:32 +0530

    added_edit_functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new;
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
   def create
-    @new_article = params[:article]
-    #u can use thii -> Article.create(title: @new_article[:title],description: @new_article[:description])
     @article = Article.new(params.require(:article).permit(:title,:description))
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -18,5 +19,14 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+  def update
+    @article = Article.find(params[:id])
+   if @article.update(params.require(:article).permit(:title,:description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+   else
+     render 'edit'
+   end
   end
 end
