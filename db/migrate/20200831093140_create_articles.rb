c3299b0b0a184fea10abc837fd95ee2725ba19a5
awpCode <rishabh.jaypee@gmail.com>
2020-08-31 15:21:44 +0530

    inital_commit_and_addedArticlesTable

---
+++ db/migrate/20200831093140_create_articles.rb
@@ -0,0 +1,18 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text "description"
+    end
+  end
+end
+
+
+#to run this migration file type
+#rails db:migrate  in the console
+
+#to generate a migration file type
+#rails generate migration create_articles
+
+#to rollback the prior migration type
+#rails db:rollback
