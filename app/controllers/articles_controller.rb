6b847b610a10e574acbafa92b8d24da6f7530fea
awpCode <rishabh.jaypee@gmail.com>
2020-09-02 13:49:36 +0530

    section-5-completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show,:edit,:update,:destroy]
+
   def home
   end
   def show
-    @article = Article.find(params[:id])
   end
   def index
     @articles = Article.all
@@ -11,10 +13,9 @@ class ArticlesController < ApplicationController
     @article = Article.new;
   end
   def edit
-    @article = Article.find(params[:id])
   end
   def create
-    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to article_path(@article)
@@ -23,8 +24,7 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-    @article = Article.find(params[:id])
-   if @article.update(params.require(:article).permit(:title,:description))
+   if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to article_path(@article)
    else
@@ -32,8 +32,21 @@ class ArticlesController < ApplicationController
    end
   end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
 end
+
+
+
+#we can reduce reducancy in new and edit views file using form partial
+#to see how visit section-4 lecture- 100
