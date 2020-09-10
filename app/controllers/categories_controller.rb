01aa5c34cf9428ee92fc8e863c4360bc25900c80
awpCode <rishabh.jaypee@gmail.com>
2020-09-11 04:01:59 +0530

    added-edit-category-function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,18 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
-
+  def edit
+    @category = Category.find(params[:id])
+  end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name was successfully edited"
+      redirect_to @category
+    else
+        render 'edit'
+    end
+  end
 
   private
 
