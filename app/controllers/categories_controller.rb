b44cfb6e25151706b67b762e38ca76bc056f0db4
awpCode <rishabh.jaypee@gmail.com>
2020-09-11 03:37:15 +0530

    addedCategoriesShowPage

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   end
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
