8ad5dbdb9d79852d6bb92f93a1f745377716afc0
awpCode <rishabh.jaypee@example.com>
2020-12-07 15:15:35 +0530

    chnages

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,6 +5,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     @category = Category.create(name: "Sports")
     @admin_user = User.create(username: "johndoe",email: "johndoe@example.com", password: "password",admin: true)
   end
+
   test "should get index" do
     get categories_url
     assert_response :success
@@ -15,6 +16,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     get new_category_url
     assert_response :success
   end
+
   test "should create category" do
     sign_in_as(@admin_user)
     assert_difference('Category.count',1) do
@@ -23,6 +25,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to category_url(Category.last)
   end
+
   test "should not create category if not admin" do
     assert_no_difference('Category.count') do
       post categories_url, params: { category: {name: "Travel"  } }
@@ -30,6 +33,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to categories_url
   end
+
   test "should show category" do
      get category_url(@category)
      assert_response :success
